e427eecdf2202697038e4fa1c023a53780160ff8
Muhammad Umer Saleem <omersaleem96@hotmail.com>
2020-04-28 15:57:37 +0500

    Project Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {LoginComponent} from './account/login/login.component';
+import {RegisterComponent} from './account/register/register.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { HomeComponent } from './customer/home/home.component';
+import { AuthGuard } from './service/AuthGuard';
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JwtModule } from "@auth0/angular-jwt";
+import {LoginComponent} from './account/login/login.component';
+import {RegisterComponent} from './account/register/register.component';
+import { FormsModule }    from '@angular/forms';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { HomeComponent } from './customer/home/home.component';
+import { AuthGuard } from './service/AuthGuard';
+export function tokenGetter() {
+  return localStorage.getItem("jwt");
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ["localhost:44378"],
+        blacklistedRoutes: []
+      }
+    })
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
